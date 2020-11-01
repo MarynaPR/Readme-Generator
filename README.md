51cf8fd94f0f8829795b16efe903c0620612640c
MarynaPR <pryadkamaryna@gmail.com>
2020-11-01 10:43:23 -0600

    write-file created

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
-# Professional README Generator Starter Code
+# README Generator
 
-[How to create a Professional README](./readme-guide.md)
+ ## Description
+
+ A well-crafted GitHub profile allows me to show off my work to other developers as well as potential employers. A quality README.md file is an extremely important component 
+ 
+ aspect of my public identity as a developer.
+ 
+  An important component of your GitHub profile—and one that many new developers often overlook—is the README.md file.
+
+ ## Technology Used
+
+ ## Web Access
+
+ Click here for the [Readme-Generator] access https://marynapr.github.io/Readme-Generator/
+
+This document was created by MarynaPR. Feel free to contact me with any questions via email pryadkamaryna@gmail.com.
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,122 +1,48 @@
+
 const badge = (license) => {
   if (license !== "none") {
     return `![license badge](https://img.shields.io/badge/license-${license}-red)`
   }
-}
-// generate markdown for README
-const generateMarkdown = projectArr => {
-  return `
-  ${projectArr
-      .filter(({ feature }) => feature)
-      .map(({ title, description, installation, usage, contribution, tests, license, questions }) => {
-        return `
-    
-  # ${data.title}
-  ## 
-  ${badge(data.license)}
-
-  # Description
-  ${data.desctiption}
-
-  # Table of Contents
-  
-  * [Installation](#installation) <br />
-
-  * [Usage](#usage) <br />
-
-  * [Contributing](#contributing) <br />
-
-  * [Tests](#tests) <br />
-
-  * [License](#license) <br />
-
-  * [Questions](#questions) <br />
-
-  ## Installation
-  ${data.installation}
-
-  ## Usage
-  ${data.usage}
-
-  ## Contribution
-  ${data.contribution}
-
-  ## Test
-  ${data.test}
-
- 
- 
-`;
-      })
-      .join('')}
-
-
-      ${projectsArr
-      .filter(({ feature }) => !feature)
-      .map(({ title, description, installation, usage, contribution, tests, license, questions }) => {
-        return `
-
-        ## License
-        ${license.join(', ')}
-
-        ## Questions
-        Please feel free to reach out to me directly with any questions @ ${data.email}. If you'd like to see my other projects, please checkout my GitHub page
-        [${data.github}](https://github.com/${data.github}/)
-        `;
-      })
-      .join('')}
-      `;
 };
 
-module.exports = templateData => {
-  //   //destructure page data by section
-  const { desctiption, table, installation, usage, contributing, tests, license, questions } = templateData;
-
+function generateMarkdown(data) {
   return `
-  ${generateMarkdown(projects)}
 
   # ${data.title}
-  ## 
+
   ${badge(data.license)}
 
-  # Description
+  ## Description
   ${data.desctiption}
 
-  # Table of Contents
-  
-  * [Installation](#installation) <br />
-
-  * [Usage](#usage) <br />
-
-  * [Contributing](#contributing) <br />
-
-  * [Tests](#tests) <br />
-
-  * [License](#license) <br />
-
-  * [Questions](#questions) <br />
-
-  ## Installation
+  ### Table of Contents
+  * [Installation](#installation) 
+  * [Usage](#usage) 
+  * [Contributing](#contributing) 
+  * [Tests](#tests) 
+  * [License](#license) 
+  * [Questions](#questions) 
+   
+  ### Installation
   ${data.installation}
 
-  ## Usage
+  ### Usage
   ${data.usage}
 
-  ## Contribution
+  ### Contribution
   ${data.contribution}
 
-  ## Test
+  ### Test
   ${data.test}
 
-  ## License
-  ${license.join(', ')}
+  ### License
+  ${data.license}
 
-  ## Questions
-  Please feel free to reach out to me directly with any questions @ ${data.email}. If you'd like to see my other projects, please checkout my GitHub page
-  [${data.github}](https://github.com/${data.github}/)
-
- `;
+  ### Questions
+  Feel free to contact me with any questions via email  @${data.email}.If you'd like to see my other projects, please checkout my GitHub page
+   [${data.github}](https://github.com/${data.github}/)
+   `;
 };
 
-//   return 
+module.exports = { generateMarkdown };
 
